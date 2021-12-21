3b0d3240363c8d054b155a2c43ab1adaea36a6e7
Craig Harshbarger <pleek91@gmail.com>
2021-12-21 09:25:39 -0600

    Make sure we listen for changes when the mediaQuery is updated

--- src/media/media.ts
+++ src/media/media.ts
@@ -10,11 +10,12 @@ export function media(query: Ref<string> | string): Ref<boolean> {
     }
 
     mediaQuery.addEventListener('change', updateMatches)
-
+    
     if(isRef(query)) {
         unwatch = watch(query, () => {
             mediaQuery.removeEventListener('change', updateMatches)
             mediaQuery = window.matchMedia(unref(query))
+            mediaQuery.addEventListener('change', updateMatches)
         })
     }
 
