d6bc69a26141f31fdab55cc2c0b29040eac4cf26
Evan Sutherland <stackoverfloweth@gmail.com>
2021-12-21 12:03:44 -0600

    additional tests written for media composition

--- tests/media/media.spec.ts
+++ tests/media/media.spec.ts
@@ -1,28 +1,70 @@
 import { media } from '@/media'
-import { isRef } from 'vue'
+import { h, isRef, ref } from 'vue'
+import { render } from '@testing-library/vue'
+import { timeout } from '../utils'
 
 describe('media', () => {
-    
-    beforeAll(() => {
-      Object.defineProperty(window, "matchMedia", {
+    const addEventListener = jest.fn()
+    const removeEventListener = jest.fn()
+  
+    beforeEach(() => {
+      Object.defineProperty(window, 'matchMedia', {
         writable: true,
         value: jest.fn().mockImplementation(query => ({
-          matches: false,
-          media: query,
-          onchange: null,
-          addListener: jest.fn(), // Deprecated
-          removeListener: jest.fn(), // Deprecated
-          addEventListener: jest.fn(),
-          removeEventListener: jest.fn(),
-          dispatchEvent: jest.fn(),
-        }))
+            matches: false,
+            media: query,
+            onchange: null,
+            addListener: jest.fn(), // deprecated
+            removeListener: jest.fn(), // deprecated
+            addEventListener,
+            removeEventListener,
+            dispatchEvent: jest.fn(),
+          }))
       });
     });
 
-    it('returns a ref', async () => {
+    afterEach(() => jest.resetAllMocks())
+    
+    it('returns a ref', async () => {       
         const match = media('(hover)')
 
         expect(isRef(match)).toBe(true)
+    })
+
+    it('if query changes, media is called again', async () => {
+        const query = ref('(hover)')
+        media(query)
+
+        query.value = '(touch)'
+        await timeout()
+        
+        expect(window.matchMedia).toBeCalledTimes(2)
+    })
+
+    it('if query changes, event listener is updated', async () => {  
+      const query = ref('(hover)')
+      media(query)
+
+      query.value = '(touch)'
+      await timeout()
+      
+      expect(removeEventListener).toBeCalledTimes(1)
+      expect(addEventListener).toBeCalledTimes(2)
+    })
+
+
+    it('if vue component is unmounted, event listener is removed', async () => {  
+      const { unmount } = render({
+        setup() {
+            media('(hover)')
+
+            return () => h('p')
+          }
+        })
+
+        unmount()
+
+        expect(removeEventListener).toBeCalledTimes(1)
     })
 
 })
