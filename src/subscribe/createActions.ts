583ea2674688d1fc776f7a0fc8cc34449353490e
Craig Harshbarger <pleek91@gmail.com>
2022-02-18 16:16:55 -0600

    Fix linting warnings from using any. Unknown is better here

--- src/subscribe/createActions.ts
+++ src/subscribe/createActions.ts
@@ -1,14 +1,17 @@
-type AnyFunction = (...args: any[]) => any
+type AnyFunction = (...args: unknown[]) => unknown
 type Callable<T> = keyof {
   [P in keyof T as T[P] extends AnyFunction ? P : never]: T[P]
 }
 
-export function createActions<T extends Record<string, any>>(context: T): Pick<T, Callable<T>> {
-  const keys = Object.keys(context).filter(key => typeof context[key] === 'function')
+export function createActions<T extends Record<string, unknown>>(context: T): Pick<T, Callable<T>> {
   const value = {} as Pick<T, Callable<T>>
 
-  return keys.reduce<Pick<T, Callable<T>>>((output, method) => {
-    output[method as Callable<T>] = context[method].bind(context)
+  return Object.keys(context).reduce<Pick<T, Callable<T>>>((output, key) => {
+    const possiblyMethod = context[key]
+
+    if (typeof possiblyMethod === 'function') {
+      output[key as Callable<T>] = possiblyMethod.bind(context)
+    }
 
     return output
   }, value)
