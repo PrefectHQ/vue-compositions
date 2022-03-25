284dbf9f52e0319221a7ba82b8d6dd10786f244b
Craig Harshbarger <pleek91@gmail.com>
2022-03-25 16:57:32 -0400

    Remove the subscribe composition in favor of useSubscription

--- src/subscribe/subscribe.ts
+++ src/subscribe/subscribe.ts
@@ -1,21 +1,15 @@
 import { getCurrentInstance, isReactive, isRef, onUnmounted, shallowReactive, unref, watch } from 'vue'
 import Manager from './manager'
 import Subscription from './subscription'
-import { Action, ActionArguments, SubscribeArguments, SubscriptionOptions } from './types'
+import { Action, ActionArguments, SubscribeArguments } from './types'
 import { watchableArgs } from './utilities'
 
 const defaultManager = new Manager()
 
-/**
- * @deprecated use useSubscription instead
- */
-export function subscribe<T extends Action>(
-  action: T,
-  args: ActionArguments<T>,
-  options: SubscriptionOptions = {},
-): Subscription<T> {
+export function useSubscription<T extends Action>(...[action, args, options = {}]: SubscribeArguments<T>): Subscription<T> {
   const manager = options.manager ?? defaultManager
-  const subscription = shallowReactive(manager.subscribe(action, args, options))
+  const argsWithDefault = args ?? ([] as unknown as ActionArguments<T>)
+  const subscription = shallowReactive(manager.subscribe(action, argsWithDefault, options))
 
   let unwatch: ReturnType<typeof watch> | undefined
 
@@ -25,7 +19,7 @@ export function subscribe<T extends Action>(
     (unref(args) as Parameters<T>).some(isRef) ||
     (unref(args) as Parameters<T>).some(isReactive)
   ) {
-    const argsToWatch = watchableArgs(args)
+    const argsToWatch = watchableArgs(argsWithDefault)
 
     unwatch = watch(
       argsToWatch,
@@ -37,7 +31,7 @@ export function subscribe<T extends Action>(
 
         subscription.unsubscribe()
 
-        const newSubscription = manager.subscribe(action, args, options)
+        const newSubscription = manager.subscribe(action, argsWithDefault, options)
 
         newSubscription.response.value ??= subscription.response.value
 
@@ -58,11 +52,4 @@ export function subscribe<T extends Action>(
   }
 
   return subscription
-}
-
-export function useSubscription<T extends Action>(...[action, args, options]: SubscribeArguments<T>): Subscription<T> {
-  const argsWithDefault = args ?? ([] as unknown as ActionArguments<T>)
-  const optionsWithDefault = options ?? {}
-
-  return subscribe(action, argsWithDefault, optionsWithDefault)
 }
