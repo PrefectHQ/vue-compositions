9be19c4d847bbecc5e505de43f411b63200173fd
Evan Sutherland <stackoverfloweth@gmail.com>
2022-06-23 21:23:42 -0500

    code review suggestion

--- src/useRouteParam/README.md
+++ src/useRouteParam/README.md
@@ -5,16 +5,13 @@ The `useRouteParam` gives you a readonly reactive value for a param. This is dif
 ```typescript
 import { useRouteParam } from '@prefecthq/vue-compositions'
 
-const foo = useRouteParam('foo') // type is `string | string[]`
-const foo = useRouteParam('foo', 'default-string') // type is `string`
-const foo = useRouteParam('foo', ['default-array']) // type is `string[]`
+const foo = useRouteParam('foo') // defaults to '' if param doesn't exist
 ```
 
 ## Arguments
 | Name         | Type                 | Default |
 |--------------|----------------------|---------|
 | param        | `string`             | None    |
-| defaultValue | `string \| string[]` | `undefined`    |
 
 ## Returns
-`string | string[]`
+`string`
--- src/useRouteParam/useRouteParam.ts
+++ src/useRouteParam/useRouteParam.ts
@@ -1,26 +1,18 @@
-/* eslint-disable no-redeclare */
 import { computed, Ref } from 'vue'
 import { useRoute } from 'vue-router'
 
-export function useRouteParam(param: string): Ref<string | string[]>
-export function useRouteParam(param: string, defaultValue: string): Ref<string>
-export function useRouteParam(param: string, defaultValue: string[]): Ref<string[]>
-export function useRouteParam(param: string, defaultValue?: string | string[]): Ref<string | string[]> {
+export function useRouteParam(param: string): Ref<string> {
   const route = useRoute()
 
   return computed(() => {
     let paramValue = route.params[param]
 
-    if (defaultValue !== undefined && Array.isArray(defaultValue) || Array.isArray(paramValue)) {
-      if (!paramValue.length) {
-        paramValue = defaultValue ?? []
-      } else {
-        [paramValue] = paramValue
-      }
+    if (Array.isArray(paramValue) && paramValue.length) {
+      [paramValue] = paramValue
     }
 
     if (!paramValue || paramValue.length === 0) {
-      paramValue = defaultValue ?? ''
+      paramValue = ''
     }
 
     return paramValue
