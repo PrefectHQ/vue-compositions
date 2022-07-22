80e44af2d53b47ba9797f0e028f0d7e8d6e3ddd8
Craig Harshbarger <pleek91@gmail.com>
2022-07-22 14:24:12 -0500

    Update useSubscriptionWithDependencies to act like useSubscription

--- src/useSubscription/useSubscriptionWithDependencies.ts
+++ src/useSubscription/useSubscriptionWithDependencies.ts
@@ -1,5 +1,5 @@
-import { reactive, Ref, toRaw, watch } from 'vue'
-import { Action, SubscriptionOptions, UseSubscription, ActionArguments } from './types'
+import { reactive, ref, Ref, toRaw, watch } from 'vue'
+import { Action, SubscriptionOptions, UseSubscription, ActionArguments, MappedSubscription } from './types'
 import { useSubscription } from './useSubscription'
 
 const voidAction = (): undefined => undefined
@@ -10,27 +10,44 @@ type UseSubscriptionWithDependencies<T extends Action> = [
   options?: SubscriptionOptions
 ]
 
+// returns a void subscription with executed overridden to be false
+function rawVoidSubscription(): MappedSubscription<typeof voidAction> {
+  const subscription = toRawSubscription(useSubscription(voidAction))
+  subscription.executed = ref(false)
+
+  return subscription
+}
+
+// toRaw doesn't get the original type so this utility correctly sets the type to a MappedSubscription
+// which is the type useSubscription returns wrapped in reactive()
+function toRawSubscription<T extends Action>(subscription: UseSubscription<T>): MappedSubscription<T> {
+  return toRaw(subscription) as unknown as MappedSubscription<T>
+}
+
 export function useSubscriptionWithDependencies<T extends Action>(...[action, args, options = {}]: UseSubscriptionWithDependencies<T>): UseSubscription<T | typeof voidAction> {
-  const subscription = reactive(toRaw(useSubscription(voidAction)))
+  const subscription = reactive(rawVoidSubscription())
 
   watch(args, (value: Parameters<T> | null, previousValue: Parameters<T> | null | undefined) => {
     if (value === null && previousValue === undefined) {
       return
     }
 
-    if (value === null) {
-      if (subscription.isSubscribed()) {
-        subscription.unsubscribe()
-      }
+    if (subscription.isSubscribed()) {
+      subscription.unsubscribe()
+    }
 
-      Object.assign(subscription, toRaw(useSubscription(voidAction)))
+    if (value === null) {
+      Object.assign(subscription, rawVoidSubscription())
 
       return
     }
 
-    const newSubscription = toRaw(useSubscription(action, args as ActionArguments<T>, options))
+    const newSubscription = toRawSubscription(useSubscription(action, args as ActionArguments<T>, options))
+    newSubscription.response.value ??= subscription.response
+    newSubscription.executed.value = newSubscription.executed.value || subscription.executed
 
     Object.assign(subscription, newSubscription)
+
   }, { immediate: true })
 
   return subscription
