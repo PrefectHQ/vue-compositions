59aef65ba3e5bc4ae4e1e6c142d138f0e8d54ca4
Evan Sutherland <stackoverfloweth@gmail.com>
2022-10-25 15:49:19 -0500

    allow null to be written to storage

--- src/useStorage/useStorage.ts
+++ src/useStorage/useStorage.ts
@@ -33,9 +33,7 @@ export function useStorage<T>(type: StorageType, key: string, defaultValue: T | 
   }
 
   watchEffect(() => {
-    if (data.value !== null) {
-      storage.set(key, data.value)
-    }
+    storage.set(key, data.value)
   })
 
   return {
