eed844e3a0ce0247de6e4eeff0983ec3d3da0e68
Craig Harshbarger <pleek91@gmail.com>
2022-12-05 23:38:32 -0600

    Simplify the optional proxy to just change the default value to undefined

--- src/useRouteQueryParams/formats/optional.ts
+++ src/useRouteQueryParams/formats/optional.ts
@@ -2,25 +2,15 @@ import { RouteParamClass } from './RouteParam'
 
 export function Optional<T>(param: RouteParamClass<T>): RouteParamClass<T | undefined> {
   return new Proxy(param, {
-    construct(target, [key]: [string]) {
-      const instance = new target(key)
+    construct(target, params: ConstructorParameters<RouteParamClass<T>>) {
+      const instance = new target(...params)
 
       return new Proxy(instance, {
         get(target, property) {
           const value = Reflect.get(target, property)
 
-          if (property === 'getSingleValue') {
-            return new Proxy(value, {
-              apply(getSingleValue, thisArg, args) {
-                const value = Reflect.apply(getSingleValue, thisArg, args)
-
-                if (value === thisArg.default) {
-                  return undefined
-                }
-
-                return value
-              },
-            })
+          if (property === 'default') {
+            return undefined
           }
 
           return value
