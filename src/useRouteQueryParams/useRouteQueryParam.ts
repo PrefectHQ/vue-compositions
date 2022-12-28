b3c9e211bb33035d15567d866f37add770b39679
Craig Harshbarger <pleek91@gmail.com>
2022-12-28 08:53:42 -0500

    Try overloading useRouteQueryParam

--- src/useRouteQueryParams/useRouteQueryParam.ts
+++ src/useRouteQueryParams/useRouteQueryParam.ts
@@ -2,11 +2,14 @@
 import { computed, Ref } from 'vue'
 import { NoInfer } from '@/types/generics'
 import { useRouteQuery } from '@/useRouteQuery/useRouteQuery'
+import { StringRouteParam } from '@/useRouteQueryParams/formats'
 import { RouteParamClass } from '@/useRouteQueryParams/formats/RouteParam'
 
+
+export function useRouteQueryParam<T>(key: string): Ref<string>
 export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: NoInfer<T>): Ref<T>
 export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: NoInfer<T>[]): Ref<T[]>
-export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: NoInfer<T> | NoInfer<T>[]): Ref<T | T[]> {
+export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T> | RouteParamClass<string> = StringRouteParam, defaultValue: NoInfer<T> | NoInfer<T>[] | string = ''): Ref<T | string | T[]> {
   const query = useRouteQuery()
   const format = new formatter(key, defaultValue)
 
