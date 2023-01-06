c39cae000a44217ec914b9d4de72224fa33c269c
Craig Harshbarger <pleek91@gmail.com>
2023-01-06 14:19:31 -0600

    Update overloads to allow for optional default values

--- src/useRouteQueryParam/useRouteQueryParam.ts
+++ src/useRouteQueryParam/useRouteQueryParam.ts
@@ -10,24 +10,17 @@ function isDefaultValue<T>(value: T | RouteParamClass): value is T {
   return !isRouteParamClass(value)
 }
 
-export function useRouteQueryParam(key: string, defaultValue?: string): Ref<string>
-export function useRouteQueryParam(key: string, defaultValue?: string | null): Ref<string | null>
-export function useRouteQueryParam(key: string, defaultValue?: string | undefined): Ref<string | undefined>
-export function useRouteQueryParam(key: string, defaultValue?: string | null | undefined): Ref<string | null | undefined>
+export function useRouteQueryParam(key: string, defaultValue?: string): Ref<string | undefined>
+export function useRouteQueryParam(key: string, defaultValue: string): Ref<string>
 export function useRouteQueryParam(key: string, defaultValue: string[]): Ref<string[]>
+export function useRouteQueryParam(key: string, defaultValue: MaybeArray<string>): Ref<MaybeArray<string>>
+export function useRouteQueryParam(key: string, defaultValue: MaybeArray<string> | undefined): Ref<MaybeArray<string> | undefined>
 export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: NoInfer<T>): Ref<T>
-export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: NoInfer<T> | null): Ref<T | null>
 export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: NoInfer<T> | undefined): Ref<T | undefined>
-export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: NoInfer<T> | null | undefined): Ref<T | null | undefined>
 export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: NoInfer<T>[]): Ref<T[]>
-export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: MaybeArray<NoInfer<T>> | null | undefined): Ref<MaybeArray<T>>
-export function useRouteQueryParam(key: string, formatterOrDefaultValue?: RouteParamClass | MaybeArray<string> | null | undefined, maybeDefaultValue?: MaybeArray | null | undefined): Ref {
-
-  const isStringParamWithNoDefaultValue = formatterOrDefaultValue === undefined
-
-  if (isStringParamWithNoDefaultValue) {
-    return useRouteQueryParam(key, '')
-  }
+export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: MaybeArray<T>): Ref<MaybeArray<T>>
+export function useRouteQueryParam<T>(key: string, formatter: RouteParamClass<T>, defaultValue: MaybeArray<T> | undefined): Ref<MaybeArray<T> | undefined>
+export function useRouteQueryParam(key: string, formatterOrDefaultValue?: RouteParamClass | MaybeArray<string> | undefined, maybeDefaultValue?: MaybeArray | undefined): Ref {
 
   const isStringParamWithDefaultValue = isDefaultValue(formatterOrDefaultValue)
 
