26e4241db37a524a34ec68a9f1eb792d93da9e99
Craig Harshbarger <pleek91@gmail.com>
2023-01-23 15:41:27 -0600

    Add some unit tests and refactor the utility file

--- src/useIsSame/useIsSame.ts
+++ src/useIsSame/useIsSame.ts
@@ -1,6 +1,6 @@
 import { computed, ComputedRef, ref } from 'vue'
 import { MaybeRef } from '@/types/maybe'
-import { isSame } from '@/utilities/variables'
+import { isSame } from '@/utilities/isSame'
 
 export function useIsSame(valueA: MaybeRef, valueB: MaybeRef): ComputedRef<boolean> {
   const valueARef = ref(valueA)
