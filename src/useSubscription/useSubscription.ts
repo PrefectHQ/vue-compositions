68298eb6722427ce57fe35e2e806df98213aa00a
Craig Harshbarger <pleek91@gmail.com>
2023-03-10 11:42:54 -0600

    Rework how subscriptions watch args to prevent resubscribing unnecessarily

--- src/useSubscription/useSubscription.ts
+++ src/useSubscription/useSubscription.ts
@@ -2,8 +2,9 @@ import { getCurrentInstance, onUnmounted, reactive, unref, watch } from 'vue'
 import Manager from '@/useSubscription/models/manager'
 import { Action, ActionArguments } from '@/useSubscription/types/action'
 import { SubscribeArguments, UseSubscription } from '@/useSubscription/types/subscription'
-import { watchableArgs } from '@/useSubscription/utilities/reactivity'
 import { mapSubscription } from '@/useSubscription/utilities/subscriptions'
+import { getValidWatchSource } from '@/utilities/getValidWatchSource'
+import { uniqueValueWatcher } from '@/utilities/uniqueValueWatcher'
 
 const defaultManager = new Manager()
 
@@ -13,36 +14,29 @@ export function useSubscription<T extends Action>(...[action, args, options = {}
   const originalSubscription = manager.subscribe(action, argsWithDefault, options)
   const subscriptionResponse = reactive(mapSubscription(originalSubscription))
 
-  let unwatch: ReturnType<typeof watch> | undefined
-  const watchable = watchableArgs(argsWithDefault)
+  const unwatch = uniqueValueWatcher(getValidWatchSource(args), () => {
+    // checking if args are null to support useSubscriptionWithDependencies
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (!subscriptionResponse.isSubscribed() || unref(argsWithDefault) === null) {
+      unwatch!()
+      return
+    }
 
-  if (watchable !== null) {
-    unwatch = watch(watchable, () => {
-      // checking if args are null to support useSubscriptionWithDependencies
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      if (!subscriptionResponse.isSubscribed() || unref(argsWithDefault) === null) {
-        unwatch!()
-        return
-      }
+    subscriptionResponse.unsubscribe()
 
-      subscriptionResponse.unsubscribe()
+    const newSubscription = manager.subscribe(action, argsWithDefault, options)
 
-      const newSubscription = manager.subscribe(action, argsWithDefault, options)
+    newSubscription.response.value ??= subscriptionResponse.response
+    newSubscription.executed.value = newSubscription.executed.value || subscriptionResponse.executed
 
-      newSubscription.response.value ??= subscriptionResponse.response
-      newSubscription.executed.value = newSubscription.executed.value || subscriptionResponse.executed
-
-      Object.assign(subscriptionResponse, mapSubscription(newSubscription))
-    }, { deep: true })
-  }
+    Object.assign(subscriptionResponse, mapSubscription(newSubscription))
+  }, { deep: true })
 
   if (getCurrentInstance()) {
     onUnmounted(() => {
       subscriptionResponse.unsubscribe()
 
-      if (unwatch) {
-        unwatch()
-      }
+      unwatch()
     })
   }
 
