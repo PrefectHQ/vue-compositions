2e904246374f6bd349a1fa5a16913896502c1532
nicholas <znicholasbrown@gmail.com>
2023-04-06 15:54:21 -0400

    Update uniqueSubscribe to always use a computed options object

--- src/utilities/tests.ts
+++ src/utilities/tests.ts
@@ -1,4 +1,4 @@
-import { isRef } from 'vue'
+import { computed, isRef, unref } from 'vue'
 import { useSubscription } from '@/useSubscription'
 import Manager from '@/useSubscription/models/manager'
 import { Action } from '@/useSubscription/types/action'
@@ -8,12 +8,15 @@ export function timeout(ms = 0): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-export function uniqueSubscribe<T extends Action>(...[action, args, options = {}]: SubscribeArguments<T>): UseSubscription<T> {
-  if (isRef(options)) {
-    options.value.manager = new Manager()
-  } else {
-    options.manager = new Manager()
-  }
+export function uniqueSubscribe<T extends Action>(...[action, args, optionsArg = {}]: SubscribeArguments<T>): UseSubscription<T> {
+  const options = computed(() => {
+    const options = unref(optionsArg)
+
+    return {
+      ...options,
+      manager: new Manager(),
+    }
+  })
 
   return useSubscription(action, args, options)
 }
