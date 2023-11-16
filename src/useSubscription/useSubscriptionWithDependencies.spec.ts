a37a9cbc30523fecac794ca907abb1807410af8d
Craig Harshbarger <pleek91@gmail.com>
2023-11-16 13:35:52 -0600

    Simplify unit test

--- src/useSubscription/useSubscriptionWithDependencies.spec.ts
+++ src/useSubscription/useSubscriptionWithDependencies.spec.ts
@@ -4,14 +4,10 @@ import { useSubscriptionWithDependencies } from '@/useSubscription/useSubscripti
 import { timeout } from '@/utilities/tests'
 
 test('it does not execute the action when args have not changed', async () => {
-  const number = ref(0)
   const object = ref({ value: 0 })
   const action = vi.fn()
 
-  const parameters = computed(() => [
-    number.value,
-    object.value,
-  ])
+  const parameters = computed(() => [object.value])
 
   useSubscriptionWithDependencies(action, parameters)
 
@@ -19,14 +15,12 @@ test('it does not execute the action when args have not changed', async () => {
 
   expect(action).toBeCalledTimes(1)
 
-  number.value = 1
   object.value = { value: 1 }
 
   await timeout()
 
   expect(action).toBeCalledTimes(2)
 
-  number.value = 1
   object.value = { value: 1 }
 
   await timeout()
