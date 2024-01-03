593b943d7394d8a3aa5aa40c74f87ba71b25537c
Collin Choy <collin@prefect.io>
2024-01-03 16:32:51 -0500

    Add a doc string for useSubscription.

--- src/useSubscription/useSubscription.ts
+++ src/useSubscription/useSubscription.ts
@@ -9,7 +9,46 @@ import { uniqueValueWatcher } from '@/utilities/uniqueValueWatcher'
 
 const defaultManager = new Manager()
 
-export function useSubscription<T extends Action>(...[action, args, optionsArg = {}]: SubscribeArguments<T>): UseSubscription<T> {
+/**
+ * The `useSubscription` composition manages data sharing across components. Multiple components can subscribe to an `action` (any method or function) and share the response value.
+ *
+ * @example
+ * ```typescript
+ * import { useSubscription } from '@prefecthq/vue-compositions'
+ *
+ * const subscription = useSubscription(action, args, options)
+ * ```
+ *
+ * ## How it works
+ * When creating a subscription for the first time a `Channel` is created. A channel is unique to both the `action` and the `args` used to create the subscription. When a channel is created it immediately executes the action with the given args. If `options.interval` is set the action will execute again at that interval updating the `loading`, `errored`, `error`, and `response` values automatically.
+ *
+ * If another subscription is created with the same action and args, it will not execute the action again. Instead the same channel will be reused. So if multiple components subscribe using the same args the action will only be executed once.
+ *
+ * If any subscriptions have `options.interval` then the shortest interval will be used. All subscriptions will received an updated response at the decided interval, even if a specific subscription has no interval or if it has a greater interval. Interval is basically "maximum age of execution" which defaults to `Infinity`.
+ *
+ * When a subscription is removed the channel interval is recalculated. A subscription can be removed by calling `subscription.unsubscribe()` or the subscription will automatically be removed when the component that created the subscription is unmounted.
+ *
+ * @see [README](https://github.com/PrefectHQ/vue-compositions/tree/main/src/useSubscription#readme)
+ *
+ * @param action - The function to be executed.
+ * @param args - Parameters of the action being executed.
+ * @param options - An object containing optional parameters.
+ * @param options.interval - A max poll interval in milliseconds.
+ * @param options.manager - A channel manager to use for the subscription.
+ *
+ * @returns An object containing the following properties:
+ * - `loading`: Set to `true` while the action is being executed.
+ * - `errored`: Set to `true` if there is an error when executing the action.
+ * - `error`: Stores any error thrown while executing the action.
+ * - `response`: Return value from the `action` after execution.
+ * - `refresh`: Executes the `action` again.
+ * - `promise`: Returns a promise that resolves when the `response` is returned.
+ * - `unsubscribe`: Remove the subscription from the channel.
+ * - `subscribe`: Resubscribes to the channel using the same arguments.
+ */
+export function useSubscription<T extends Action>(
+  ...[action, args, optionsArg = {}]: SubscribeArguments<T>
+): UseSubscription<T> {
   const options = unref(optionsArg)
   const manager = options.manager ?? defaultManager
   const argsWithDefault = args ?? [] as unknown as ActionArguments<T>
