b4a8a53dd760231aeab9978650d8cf0a4cdb3f84
Collin Choy <collin@prefect.io>
2024-01-03 19:11:00 -0500

    Shorten useSubscription doc string.

--- src/useSubscription/useSubscription.ts
+++ src/useSubscription/useSubscription.ts
@@ -12,23 +12,7 @@ const defaultManager = new Manager()
 /**
  * The `useSubscription` composition manages data sharing across components. Multiple components can subscribe to an `action` (any method or function) and share the response value.
  *
- * @example
- * ```typescript
- * import { useSubscription } from '@prefecthq/vue-compositions'
- *
- * const subscription = useSubscription(action, args, options)
- * ```
- *
- * ## How it works
- * When creating a subscription for the first time a `Channel` is created. A channel is unique to both the `action` and the `args` used to create the subscription. When a channel is created it immediately executes the action with the given args. If `options.interval` is set the action will execute again at that interval updating the `loading`, `errored`, `error`, and `response` values automatically.
- *
- * If another subscription is created with the same action and args, it will not execute the action again. Instead the same channel will be reused. So if multiple components subscribe using the same args the action will only be executed once.
- *
- * If any subscriptions have `options.interval` then the shortest interval will be used. All subscriptions will received an updated response at the decided interval, even if a specific subscription has no interval or if it has a greater interval. Interval is basically "maximum age of execution" which defaults to `Infinity`.
- *
- * When a subscription is removed the channel interval is recalculated. A subscription can be removed by calling `subscription.unsubscribe()` or the subscription will automatically be removed when the component that created the subscription is unmounted.
- *
- * @see [README](https://github.com/PrefectHQ/vue-compositions/tree/main/src/useSubscription#readme)
+ * @see [docs](https://github.com/PrefectHQ/vue-compositions/tree/main/src/useSubscription#readme)
  *
  * @param action - The function to be executed.
  * @param args - Parameters of the action being executed.
