f493340b944a73a5f4b5b24aaf1409fcc0947369
Collin Choy <collin@prefect.io>
2024-01-17 12:18:19 -0500

    Convert devtools entrypoint to a plugin.

--- src/devtools.ts
+++ src/devtools.ts
@@ -2,19 +2,22 @@ import {
   type App,
   setupDevtoolsPlugin
 } from '@vue/devtools-api'
+import { type Plugin } from 'vue'
 import * as useSubscriptionDevtools from './useSubscription/useSubscriptionDevtools'
 
-export function setupDevtools(app: App): void {
-  setupDevtoolsPlugin({
-    id: 'prefect-vue-compositions-devtools',
-    label: 'Prefect Devtools',
-    packageName: '@prefecthq/vue-compositions',
-    homepage: 'https://www.prefect.io/',
-    settings: {
-      ...useSubscriptionDevtools.SUBSCRIPTION_DEVTOOLS_SETTINGS,
-    },
-    app,
-  }, (api) => {
-    useSubscriptionDevtools.init(api)
-  })
+export const plugin: Plugin = {
+  install(app: App): void {
+    setupDevtoolsPlugin({
+      id: 'prefect-vue-compositions-devtools',
+      label: 'Prefect Devtools',
+      packageName: '@prefecthq/vue-compositions',
+      homepage: 'https://www.prefect.io/',
+      settings: {
+        ...useSubscriptionDevtools.SUBSCRIPTION_DEVTOOLS_SETTINGS,
+      },
+      app,
+    }, (api) => {
+      useSubscriptionDevtools.init(api)
+    })
+  }
 }
