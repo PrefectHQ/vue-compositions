6dff5cdb2b2b935fb7e6ca0e4cf650d5af0480f8
nicholas <znicholasbrown@gmail.com>
2024-03-15 16:12:07 -0400

    Add some teardown/spinup logic

--- src/useMousePosition/useMousePosition.ts
+++ src/useMousePosition/useMousePosition.ts
@@ -1,4 +1,4 @@
-import { reactive } from 'vue'
+import { onScopeDispose, reactive, ref } from 'vue'
 import { useGlobalEventListener } from '@/useGlobalEventListener'
 
 export type MousePosition = {
@@ -27,11 +27,39 @@ const updateMousePosition = (event: MouseEvent): void => {
   }
 }
 
-useGlobalEventListener('mousemove', updateMousePosition, { passive: true })
-useGlobalEventListener('click', updatePositionAtLastClick, { capture: true })
-useGlobalEventListener('contextmenu', updatePositionAtLastClick, { capture: true })
+const listeners = ref(0)
+
+const { add: addMouseMoveEventListener, remove: removeMouseMoveEventListener } = useGlobalEventListener('mousemove', updateMousePosition, { passive: true })
+const { add: addClickEventListener, remove: removeClickEventListener } = useGlobalEventListener('click', updatePositionAtLastClick, { capture: true })
+const { add: addContextMenuEventListener, remove: removeContextMenuEventListener } = useGlobalEventListener('contextmenu', updatePositionAtLastClick, { capture: true })
+
+function tryTeardownEventListeners(): void {
+  if (listeners.value > 0) {
+    return
+  }
+
+  removeMouseMoveEventListener()
+  removeClickEventListener()
+  removeContextMenuEventListener()
+}
+
+function addEventListeners(): void {
+  // These have no effect if the event listeners are already added
+  addMouseMoveEventListener()
+  addClickEventListener()
+  addContextMenuEventListener()
+}
 
 export function useMousePosition(): UseMousePosition {
+  listeners.value += 1
+
+  addEventListeners()
+
+  onScopeDispose(() => {
+    listeners.value -= 1
+    tryTeardownEventListeners()
+  })
+
   return {
     position,
     positionAtLastClick,
